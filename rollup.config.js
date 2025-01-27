da43af1ebfb422b757b0c200a4d4d1eecd95a1fb
ntr75535 <ntr75535@gmail.com>
2025-01-27 17:59:14 +0700

    Create rollup.config.js

---
+++ rollup.config.js
@@ -0,0 +1,17 @@
+import typescript from '@rollup/plugin-typescript';
+import commonjs from '@rollup/plugin-commonjs';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+
+export default {
+  input: 'src/index.ts',
+  output: {
+    file: 'dist/bundle.js',
+    format: 'iife', // Immediately Invoked Function Expression
+    sourcemap: true
+  },
+  plugins: [
+      nodeResolve({browser:true}),
+      commonjs(),
+        typescript({tsconfig:'./tsconfig.json'})
+  ],
+};
