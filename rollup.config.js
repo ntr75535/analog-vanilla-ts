ed500b04e041b46b02a8c98dfedcff4102720ecf
ntr75535 <ntr75535@gmail.com>
2025-01-27 18:46:12 +0700

    Update rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -1,17 +1,25 @@
-import typescript from '@rollup/plugin-typescript';
-import commonjs from '@rollup/plugin-commonjs';
+import typescript from '@rollup/plugin-typescript2';
+import babel from '@rollup/plugin-babel';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
 
 export default {
-  input: 'src/index.ts',
-  output: {
-    file: 'dist/bundle.js',
-    format: 'iife', // Immediately Invoked Function Expression
-    sourcemap: true
-  },
-  plugins: [
-      nodeResolve({browser:true}),
-      commonjs(),
-      typescript({tsconfig:'./tsconfig.json'})
-  ],
+    input: 'src/index.ts',
+    output: {
+        file: 'dist/bundle.js',
+        format: 'iife', // Or 'es' for ES module
+        sourcemap: true, // Source map for debugging
+    },
+    plugins: [
+        typescript({ useTsconfigDeclarationDir: true }),
+        nodeResolve({
+            browser: true, // or node
+        }),
+        commonjs(),
+        babel({
+            babelHelpers: 'runtime',
+            exclude: 'node_modules/**',
+            extensions: ['.js', '.ts']
+        })
+    ],
 };
